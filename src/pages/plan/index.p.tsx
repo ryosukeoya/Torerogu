2bb0a5de24b70615b525d7a18e1913b30e1da996
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-03-09 21:33:54 +0900

    目標体重の設定バリデーション実装

--- src/pages/plan/index.p.tsx
+++ src/pages/plan/index.p.tsx
@@ -6,29 +6,57 @@ import { headerTabIndexAtom } from '../../store';
 import { useRecoilValue } from 'recoil';
 import { WEEK_DAYS } from '../../constants';
 import { templates } from '../../styles/template';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 const titles = ['曜日', 'カテゴリ', '種目', 'セット数', '回数'];
 
+type PlanBodyInfoFormValues = {
+  date: Date;
+  weight: number;
+};
+
 const Plan: NextPage<VFC> = () => {
   const activeIndex = useRecoilValue(headerTabIndexAtom);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<any>();
+
+  const registerPlan: SubmitHandler<PlanBodyInfoFormValues> = (data) => {
+    console.log('register!!');
+    console.log(data);
+  };
 
   if (activeIndex === 0) {
     return (
       <>
-        <div css={templates.contentArea}>
-          <h2 css={templates.title}>✏️ 目標体重を設定する</h2>
-          <div css={templates.content}>
-            <p>日付</p>
-            <Input type={'isInput'} typeAttr='date' _css={inputStyle} />
+        <form onSubmit={handleSubmit(registerPlan)}>
+          <div css={templates.contentArea}>
+            <h2 css={templates.title}>✏️ 目標体重を設定する</h2>
+            <div css={templates.content}>
+              <p>日付</p>
+              {/* <Input type={'isInput'} typeAttr='date' _css={inputStyle} /> */}
+              <input {...register('date', { required: true })} type='date' css={inputStyle} />
+              <p css={templates.errorMessage}>
+                {errors.date?.type === 'required' && '必須項目です'}
+              </p>
+            </div>
+            <div css={templates.content}>
+              <p>体重</p>
+              {/* <Input type={'isInput'} typeAttr='text' placeholder='60' _css={inputStyle} /> */}
+              <input {...register('weight', { required: true, maxLength: 3, pattern: /[0-9]/ })} placeholder={'60'} css={inputStyle} />
+              <span>kg</span>
+              <p css={templates.errorMessage}>
+                {errors.weight?.type === 'required' && '必須項目です'}
+                {errors.weight?.type === 'pattern' && '数値を入力してください'}
+                {errors.weight?.type === 'maxLength' && '3桁以下にしてください'}
+              </p>
+            </div>
           </div>
-          <div css={templates.content}>
-            <p>体重</p>
-            <Input type={'isInput'} typeAttr='text' placeholder='60' _css={inputStyle} />
-            kg
-          </div>
-        </div>
-
-        <Button type={'isButton'} text={'記録する'} _css={simpleButton(10)} />
+          {/* <Button type={'isButton'} text={'記録する'} _css={simpleButton(10)} /> */}
+          <Input type={'isInput'} typeAttr='submit' _css={simpleButton(10)} value={'記録する'} />
+        </form>
       </>
     );
   } else if (activeIndex === 1) {
