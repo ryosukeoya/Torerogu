a9c5bdc21d6d34fdd3273609310f0f7a238d1467
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-03-09 21:15:05 +0900

    ヘッダーのタイトルクリックでホームに戻る機能一部不具合があったので修正

--- src/components/BottomNav/index.tsx
+++ src/components/BottomNav/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import React, { VFC, useState } from 'react';
+import React, { VFC, useState, useEffect } from 'react';
 import Tab from './BottomNavTab';
 import { PAGE_PATH } from '../../constants/index';
 import type { PageTitles, PageTitle } from '../../types/index';
@@ -9,10 +9,14 @@ import { BORDER } from '../../styles/const';
 
 const BottomNav: VFC = () => {
   const router = useRouter();
-  const initialIndex = getPathIndex(router.pathname);
+  const pathIndex = getPathIndex(router.pathname);
+  const [activeIndex, setActiveIndex] = useState<number>(pathIndex);
 
-  const [activeIndex, setActiveIndex] = useState<number>(initialIndex);
   const titles = Object.keys(PAGE_PATH) as PageTitles;
+
+  useEffect(() => {
+    setActiveIndex(pathIndex);
+  }, [pathIndex]);
 
   return (
     <nav css={styles.bottomNav}>
