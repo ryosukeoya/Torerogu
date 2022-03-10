10a10b68ce2dd6522f4ce52a0193b774d9f6dd7c
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-03-11 05:59:54 +0900

    デザイン微調整

--- src/components/uiParts/Select/style.tsx
+++ src/components/uiParts/Select/style.tsx
@@ -4,6 +4,7 @@ import { BORDER } from '../../../styles/const';
 export const selectStyle = (marginBottom?: number, customCss?: SerializedStyles) => css`
   width: 200px;
   border: 1px solid ${BORDER.GRAY};
+  padding:1px;
   margin-bottom: ${marginBottom}px;
   ${customCss};
 `;
