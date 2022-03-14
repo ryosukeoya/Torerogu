804a4b79808ba26cd3af4d2534cac8949da20cee
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-03-15 03:28:34 +0900

    pcの場合はheaderがscroll downで隠れないように修正

--- src/hooks/useIsScrollDown.ts
+++ src/hooks/useIsScrollDown.ts
@@ -1,20 +1,23 @@
 import { useState, useEffect } from 'react';
+import { isMobile } from 'react-device-detect';
 
 const useIsScrollDown = () => {
   const [position, setPosition] = useState(process.browser && window.pageYOffset);
-  const [isScrollDown, setIsScrollDown] = useState(true);
+  const [isScrollDown, setIsScrollDown] = useState(false);
 
   useEffect(() => {
-    const handleScroll = () => {
-      const moving = window.pageYOffset;
+    if (isMobile) {
+      const handleScroll = () => {
+        const moving = window.pageYOffset;
 
-      setIsScrollDown(position > moving);
-      setPosition(moving);
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
+        setIsScrollDown(moving > position);
+        setPosition(moving);
+      };
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }
   });
   return isScrollDown;
 };
