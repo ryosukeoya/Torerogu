e3954c81c6e07e3f310946eeb86e13c0b6e48742
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-03-16 01:46:44 +0900

    リファクタリング

--- src/pages/graph/index.p.tsx
+++ src/pages/graph/index.p.tsx
@@ -7,19 +7,7 @@ import { COLOR } from '../../styles/const';
 import { useQuery } from '@apollo/client';
 import { GET_TRAINING_WITH_BODY_INFO } from '../../libs/graphql/queries';
 import type { GetTrainingWithBodyInfoQuery } from '../../types/generated/graphql';
-
-type BodyInfoDataHistory = {
-  __typename?: 'body_info_data_histories' | undefined;
-  id: number;
-  user_id: number;
-  weight: number;
-  date: Date;
-};
-
-type ChartData = {
-  date: number;
-  weight: number;
-};
+import { getSortDate } from './logic';
 
 const Graph: VFC = () => {
   const { data, error } = useQuery<GetTrainingWithBodyInfoQuery>(GET_TRAINING_WITH_BODY_INFO);
@@ -27,34 +15,12 @@ const Graph: VFC = () => {
 
   if (error) return <p>Error: {error.message}</p>;
 
-  const convert = (data: BodyInfoDataHistory[] | undefined) => {
-    const result = data?.map((d: BodyInfoDataHistory) => {
-      return {
-        date: new Date(d.date).getTime(),
-        weight: d.weight,
-      };
-    });
-
-    result?.sort(function (a: ChartData, b: ChartData) {
-      if (a.date === b.date) {
-        return 0;
-      } else if (a.date < b.date) {
-        return 1;
-      } else {
-        return -1;
-      }
-    });
-
-    return result;
-  };
-
   if (activeIndex === 0) {
     return (
       <>
         <Space height={50} />
         <ResponsiveContainer width='100%' height={400}>
-          <LineChart data={convert(data?.body_info_data_histories)} margin={{ top: 5, right: 30, bottom: 5, left: 0 }}>
-            {/* <LineChart data={dummyData} margin={{ top: 5, right: 30, bottom: 5, left: 0 }}> */}
+          <LineChart data={getSortDate(data?.body_info_data_histories)} margin={{ top: 5, right: 30, bottom: 5, left: 0 }}>
             <Line type='monotone' dataKey='weight' stroke={COLOR.ORANGE} strokeWidth={2.5} dot={{ stroke: COLOR.ORANGE, strokeWidth: 2 }} />
             <CartesianGrid strokeDasharray='3 5' />
             {/* domain:横軸の表示幅を指定、tickFormatter:横軸の表示形式を指定、label:横軸のラベルを指定 */}
@@ -66,9 +32,9 @@ const Graph: VFC = () => {
       </>
     );
   } else if (activeIndex === 1) {
-    return <p>hoge</p>;
+    return <p>作成中</p>;
   } else if (activeIndex === 2) {
-    return <p>foo</p>;
+    return <p>作成中</p>;
   } else {
     return null;
   }
---
+++ src/pages/graph/logic.ts
@@ -0,0 +1,36 @@
+import type { GetTrainingWithBodyInfoQuery } from '../../types/generated/graphql';
+
+type BodyInfo = Pick<GetTrainingWithBodyInfoQuery['body_info_data_histories'][number], '__typename' | 'id' | 'user_id'>;
+
+//TODO:FIX
+type FixType = {
+  weight: number;
+  date: Date;
+};
+
+type BodyInfoDataHistory = FixType & BodyInfo;
+
+type ChartData = {
+  date: number;
+  weight: number;
+};
+export const getSortDate = (data: BodyInfoDataHistory[] | undefined): { date: number; weight: number }[] | undefined => {
+  const result = data?.map((d: BodyInfoDataHistory) => {
+    return {
+      date: new Date(d.date).getTime(),
+      weight: d.weight,
+    };
+  });
+
+  result?.sort(function (a: ChartData, b: ChartData) {
+    if (a.date === b.date) {
+      return 0;
+    } else if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+
+  return result;
+};
