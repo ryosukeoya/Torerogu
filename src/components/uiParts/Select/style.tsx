68ba94f0fbf411387e8abff0278d6b5b5a9327f3
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-03-17 09:09:20 +0900

    セレクトボックスのcss修正

--- src/components/uiParts/Select/style.tsx
+++ src/components/uiParts/Select/style.tsx
@@ -1,10 +1,18 @@
 import { css, SerializedStyles } from '@emotion/react';
 import { BORDER } from '../../../styles/const';
 
+const disableDefaultStyle = css`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+`;
+
 export const selectStyle = (marginBottom?: number, customCss?: SerializedStyles) => css`
+  ${disableDefaultStyle}
   width: 200px;
   border: 1px solid ${BORDER.GRAY};
-  padding:1px;
+  padding: 1px;
   margin-bottom: ${marginBottom}px;
+  background: url(/imgs/down-arrow.png) no-repeat right 10px center / 16px auto;
   ${customCss};
 `;
