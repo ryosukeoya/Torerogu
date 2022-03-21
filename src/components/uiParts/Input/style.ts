e40bca00cbf32f8f4f34cccd021922daf175f5f4
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-03-22 05:21:53 +0900

    修正

--- src/components/uiParts/Input/style.ts
+++ src/components/uiParts/Input/style.ts
@@ -44,8 +44,9 @@ export const textareaStyle = (customCss?: SerializedStyles) => css`
 
 export const submitStyle = () => css`
   display: block;
-  border: none;
   width: 100%;
   height: 100%;
   padding: 13px 0;
+  border: 0;
+  border-radius: 22px;
 `;
