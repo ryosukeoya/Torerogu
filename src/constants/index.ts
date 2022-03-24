96ed5c5b4b277ceec3e265b7c565b38643037dd9
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-03-24 14:09:26 +0900

    リファクタリング

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,11 +1,9 @@
-import { PageTitles } from '../types/index';
-
 export const APP = {
   NAME: 'トレログ',
   DESC: '筋トレアプリ',
 } as const;
 
-export const PAGE_TITLE = ['ホーム', '計画', '記録', 'グラフ'] as PageTitles;
+export const PAGE_TITLE = ['ホーム', '計画', '記録', 'グラフ'] as const;
 
 export const PAGE_PATH = {
   [PAGE_TITLE[0]]: '/',
