ee04d75d33f90940c6e966585feb109c3bba6595
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-03-24 15:05:02 +0900

    修正

--- src/components/style.ts
+++ src/components/style.ts
@@ -1,4 +1,4 @@
 export { cardStyle } from './uiParts/Card/style';
-export { inputStyle } from './uiParts/InputPart/style';
 export { sliderStyle } from './uiParts/Slider/style';
+export { inputStyle } from './uiParts/FormItems/InputPart/style';
 export { selectStyle } from './uiParts/FormItems/SelectPart/style';
