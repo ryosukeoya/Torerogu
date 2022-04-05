2d83f8e974ea495bfa0ce10482254b15124465e9
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-04-05 22:07:10 +0900

    リファクタリング

--- src/pages/record/TrainingPage.tsx
+++ src/pages/record/TrainingPage.tsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import type { VFC } from 'react';
 import type { GetTrainingCategoryWithTypeQuery, CreateTrainingMutation } from '~/types/generated/graphql';
 import { CREATE_TRAINING } from '~/libs/graphql/mutations';
-import { Slider, SelectField, Card, FormWrapper } from '~/components';
+import { FormContainer, Slider, SelectField, Card } from '~/components';
 import { pageTemplate } from '~/styles/share/pageTemplate';
-import { SubmitHandler, useForm, FormProvider } from 'react-hook-form';
+import { SubmitHandler } from 'react-hook-form';
 import { useMutation } from '@apollo/client';
 import { css } from '@emotion/react';
 import { getNumArr, getTrainingTypesFromCategoryIndex } from '~/utils';
@@ -27,10 +27,6 @@ const TrainingPage: VFC<Props> = ({ data, pageIndex }) => {
   const [selectedCategoryIndex, setSelectedCategoryIndex] = useState<number>(0);
   const [selectedTrainingType, setSelectedTrainingType] = useState<TrainingType | null>(null);
 
-  const method = useForm<TrainingFormValues>();
-
-  const { handleSubmit } = method;
-
   const [insertTraining, { error }] = useMutation<CreateTrainingMutation>(CREATE_TRAINING, {
     onCompleted: () => setOpen(true),
   });
@@ -43,34 +39,29 @@ const TrainingPage: VFC<Props> = ({ data, pageIndex }) => {
     insertTraining({ variables: { user_id: 1, training_type_id: selectedTrainingType?.id, training_weight: data.trainingWeight, training_count: data.count, training_set: data.count, is_finish: true, date: new Date() } });
   };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   if (error) return <p>記録に失敗しました、もう一度実行してください</p>;
 
   if (selectedTrainingType) {
     //TODO:回数ではない場合、ex.ランニング
     return (
-      <FormProvider {...method}>
-        <FormWrapper
-          pageIndex={pageIndex}
-          handleSubmit={handleSubmit}
-          submitFunc={registerTraining}
-          title={`✏️ ${selectedTrainingType.name}`}
-          open={open}
-          handleClose={handleClose}
-          lastElm={
-            <p css={pageTemplate.back} onClick={() => setSelectedTrainingType(null)}>
-              ＜ カテゴリ選択に戻る
-            </p>
-          }
-        >
-          <SelectField formConf={{ name: 'trainingWeight', option: { required: true } }} title={'重量 (kg)'} texts={getNumArr(10, 200, 5)} marginBottom={42} isRequired />
-          <SelectField formConf={{ name: 'count', option: { required: true } }} title={'回数'} texts={getNumArr(1, 100, 1)} marginBottom={42} isRequired />
-          <SelectField formConf={{ name: 'set', option: { required: true } }} title={'セット数'} texts={getNumArr(1, 30, 1)} marginBottom={42} isRequired />
-        </FormWrapper>
-      </FormProvider>
+      <FormContainer<TrainingFormValues>
+        pageIndex={pageIndex}
+        submitFunc={registerTraining}
+        title={`✏️ ${selectedTrainingType.name}`}
+        open={open}
+        handleClose={() => {
+          setOpen(false);
+        }}
+        lastElm={
+          <p css={pageTemplate.back} onClick={() => setSelectedTrainingType(null)}>
+            ＜ カテゴリ選択に戻る
+          </p>
+        }
+      >
+        <SelectField formConf={{ name: 'trainingWeight', option: { required: true } }} title={'重量 (kg)'} texts={getNumArr(10, 200, 5)} marginBottom={42} isRequired />
+        <SelectField formConf={{ name: 'count', option: { required: true } }} title={'回数'} texts={getNumArr(1, 100, 1)} marginBottom={42} isRequired />
+        <SelectField formConf={{ name: 'set', option: { required: true } }} title={'セット数'} texts={getNumArr(1, 30, 1)} marginBottom={42} isRequired />
+      </FormContainer>
     );
   } else {
     return (
