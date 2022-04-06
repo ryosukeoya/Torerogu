4e8bb666189b834d0719f1d55392ab1582bfafbc
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-04-06 15:31:57 +0900

    リファクタリング

--- src/hooks/useActiveIndexFromPath.ts
+++ src/hooks/useActiveIndexFromPath.ts
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
-import { getPathIndex } from '../enum';
+import { getPathIndex } from '../utils/enums';
 
 const useActiveIndexFromPath = () => {
   const router = useRouter();
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 import { PAGE_PATH } from '../constants/index';
 
 export type PageTitle = keyof typeof PAGE_PATH;
+export type PagePaths = typeof PAGE_PATH[keyof typeof PAGE_PATH];
