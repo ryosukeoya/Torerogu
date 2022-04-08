8a7125de3f97252b9ea30273a3cc8e813dd55d44
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-04-08 16:29:36 +0900

    Snackbar表示修正

--- src/components/uiParts/Snackbar/Snackbar.tsx
+++ src/components/uiParts/Snackbar/Snackbar.tsx
@@ -2,7 +2,6 @@ import React, { forwardRef } from 'react';
 import type { VFC } from 'react';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert, { AlertProps } from '@mui/material/Alert';
-import { getCustomCss } from './func';
 
 type Props = {
   text: string;
@@ -15,9 +14,9 @@ const Alert = forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) 
   return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />;
 });
 
-const SnackbarWrap: VFC<Props> = ({ text, open, handleClose, pageIndex }) => {
+const SnackbarWrap: VFC<Props> = ({ text, open, handleClose }) => {
   return (
-    <Snackbar open={open} anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }} autoHideDuration={3000} onClose={handleClose} sx={() => getCustomCss(pageIndex)}>
+    <Snackbar open={open} anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }} autoHideDuration={3000} onClose={handleClose}>
       <Alert onClose={handleClose} severity='success' sx={{ width: '100%' }}>
         {text}
       </Alert>
--- src/components/uiParts/Snackbar/func.ts
+++ src/components/uiParts/Snackbar/func.ts
@@ -1,4 +1,4 @@
-export const getCustomCss = (pageIndex: number) => {
+export const getFixRayoutFromIndex = (pageIndex: number) => {
   if (pageIndex !== 0) {
     return { left: `calc(${pageIndex}00vw + 8px)`, right: `calc(-${pageIndex}00vw + 8px)` };
   } else {
