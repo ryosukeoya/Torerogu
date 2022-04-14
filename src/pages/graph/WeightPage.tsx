719d4dce6097809161b5d725bae889e9e54fdc66
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-04-14 17:13:07 +0900

    グラフ表示期間変更機能作成

--- src/pages/graph/WeightPage.tsx
+++ src/pages/graph/WeightPage.tsx
@@ -1,8 +1,8 @@
-import React, { VFC } from 'react';
+import React, { useState, VFC } from 'react';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, ResponsiveContainer } from 'recharts';
-import { PrimaryNavigationLocalState } from '~/components';
+import { PrimaryNavigationPresenter } from '~/components';
 import { COLOR } from '~/styles/const';
-import { getSortDate } from './logic';
+import { getSortedDataFromDate, getDataExtractionInSpecifiedPeriod, getSpecifyDatePeriodFromActiveIndex } from './logic';
 import type { GetTrainingWithBodyInfoQuery } from '~/types/generated/graphql';
 import { pageTemplate } from '~/styles/shares';
 import { css } from '@emotion/react';
@@ -13,7 +13,14 @@ type Props = {
   bodyInfo: GetTrainingWithBodyInfoQuery['body_info_data_histories'] | undefined;
 };
 
+type BodyInfo = GetTrainingWithBodyInfoQuery['body_info_data_histories'];
+
 const WeightPage: VFC<Props> = ({ bodyInfo }) => {
+  const [activeIndex, setActiveIndex] = useState<number>(titles.length - 1);
+
+  // TODO
+  // 表示するデータがない時データがないことをわかるようにしないと
+
   return (
     <div
       css={[
@@ -24,7 +31,12 @@ const WeightPage: VFC<Props> = ({ bodyInfo }) => {
       ]}
     >
       <ResponsiveContainer width='100%' height={400}>
-        <LineChart data={getSortDate(bodyInfo)} margin={{ top: 5, right: 30, bottom: 5, left: 0 }}>
+        <LineChart
+          data={bodyInfo && getSortedDataFromDate(getDataExtractionInSpecifiedPeriod<Required<BodyInfo>>(bodyInfo, getSpecifyDatePeriodFromActiveIndex(activeIndex)))}
+          margin={{ top: 5, right: 30, bottom: 5, left: 0 }}
+          //
+        >
+          {/* <LineChart data={getSortedDataFromDate(getDataExtractionInSpecifiedPeriod(bodyInfo, 0))} margin={{ top: 5, right: 30, bottom: 5, left: 0 }}> */}
           <Line type='monotone' dataKey='weight' stroke={COLOR.ORANGE} strokeWidth={2.5} dot={{ stroke: COLOR.ORANGE, strokeWidth: 2 }} />
           <CartesianGrid strokeDasharray='3 5' />
           {/* domain:横軸の表示幅を指定、tickFormatter:横軸の表示形式を指定、label:横軸のラベルを指定 */}
@@ -32,7 +44,8 @@ const WeightPage: VFC<Props> = ({ bodyInfo }) => {
           <YAxis />
         </LineChart>
       </ResponsiveContainer>
-      <PrimaryNavigationLocalState titles={titles} theme='roundish' options={{ isToggle: true, isSwiper: false }} initialActiveIndex={titles.length - 1} />
+      {/* <PrimaryNavigationLocalState titles={titles} theme='roundish' options={{ isToggle: true, isSwiper: false }} initialActiveIndex={titles.length - 1} /> */}
+      <PrimaryNavigationPresenter titles={titles} theme='roundish' options={{ isToggle: true, isSwiper: false }} activeIndex={activeIndex} setActiveIndex={setActiveIndex} />
     </div>
   );
 };
--- src/pages/graph/logic.ts
+++ src/pages/graph/logic.ts
@@ -1,21 +1,16 @@
-import type { GetTrainingWithBodyInfoQuery } from '~/types/generated/graphql';
-
-type BodyInfo = Pick<GetTrainingWithBodyInfoQuery['body_info_data_histories'][number], '__typename' | 'id' | 'user_id'>;
-
-//TODO:FIX
-type FixType = {
+interface PreChartData extends Record<string, unknown> {
+  date: string;
   weight: number;
-  date: Date;
-};
-
-type BodyInfoDataHistory = FixType & BodyInfo;
+}
 
 type ChartData = {
   date: number;
   weight: number;
 };
-export const getSortDate = (data: BodyInfoDataHistory[] | undefined): { date: number; weight: number }[] | undefined => {
-  const result = data?.map((d: BodyInfoDataHistory) => {
+
+// 日付でSortしたものを返す
+export const getSortedDataFromDate = (data: PreChartData[]): ChartData[] => {
+  const result = data?.map((d) => {
     return {
       date: new Date(d.date).getTime(),
       weight: d.weight,
@@ -33,4 +28,35 @@ export const getSortDate = (data: BodyInfoDataHistory[] | undefined): { date: nu
   });
 
   return result;
+};
+
+// 指定した期間でデータを抽出する
+export const getDataExtractionInSpecifiedPeriod = <T extends { date: string }[] & { [key: string]: unknown }[]>(data: T, specifyDatePeriod: 'all' | 7 | 30 | 365): T => {
+  if (specifyDatePeriod === 'all') return data;
+  const currentDate = new Date();
+  const date = new Date();
+  date.setDate(date.getDate() - specifyDatePeriod);
+  const extractedData = data?.filter((d) => {
+    const di = new Date(d.date);
+    return date <= di && di <= currentDate;
+  });
+  return extractedData as T;
+};
+
+export const getSpecifyDatePeriodFromActiveIndex = (activeIndex: number): 'all' | 7 | 30 | 365 => {
+  switch (activeIndex) {
+    // one week
+    case 0:
+      return 7;
+    // one month
+    case 1:
+      return 30;
+    // one year
+    case 2:
+      return 365;
+    case 3:
+      return 'all';
+    default:
+      throw new Error('wrong activeIndex given');
+  }
 };
