b9f02db783169a5d7abd50ee569356387af86ba5
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-04-18 17:25:56 +0900

    リファクタリング

--- src/types/app.ts
+++ src/types/app.ts
@@ -2,3 +2,4 @@ import { PAGE_PATH } from '../constants';
 
 export type PageTitle = keyof typeof PAGE_PATH;
 export type PagePaths = typeof PAGE_PATH[keyof typeof PAGE_PATH];
+// export type Page = { [key in PageTitle]: string };
--- src/utils/app.tsx
+++ src/utils/app.tsx
@@ -3,6 +3,86 @@ import Image from 'next/image';
 import { PAGE_TITLE, PAGE_PATH, WEEK_DAYS } from '../constants';
 import type { PageTitle } from '~/types';
 
+// TODO: FATすぎるファイル分けた方がいい
+
+// 指定した日付のデータを取得する
+export const getDataSpecifiedDate = <T extends { date: string }[] & { [key: string]: unknown }[]>(data: T, date: Date) => {
+  return data?.filter((d) => d.date === getStringTypeDate(date, 'YYYY-MM-DD')) as T;
+};
+
+interface PreChartData extends Record<string, unknown> {
+  date: string;
+  weight: number;
+}
+
+type ChartData = {
+  date: number;
+  weight: number;
+};
+
+const GRAPH_PERIOD = {
+  WEEK: 7,
+  MONTH: 30,
+  YEAR: 365,
+  ALL: 'all',
+} as const;
+
+type GraphPeriodValue = typeof GRAPH_PERIOD[keyof typeof GRAPH_PERIOD];
+
+// 指定した期間でデータを抽出したものを返す
+export const getDataExtractionInSpecifiedPeriod = <T extends { date: string; is_record: boolean }[] & { [key: string]: unknown }[]>(data: T, specifyDatePeriod: GraphPeriodValue): T => {
+  const currentDate = new Date();
+  const date = new Date();
+  specifyDatePeriod !== 'all' && date.setDate(date.getDate() - specifyDatePeriod);
+  const extractedData = data?.filter((d) => {
+    if (specifyDatePeriod === 'all') {
+      return d.is_record === true;
+    } else {
+      const di = new Date(d.date);
+      return date <= di && di <= currentDate && d.is_record === true;
+    }
+  });
+  return extractedData as T;
+};
+
+// 日付でソートしたものを返す
+export const getSortedDataFromDate = (data: PreChartData[]): ChartData[] => {
+  const result = data?.map((d) => {
+    return {
+      date: new Date(d.date).getTime(),
+      weight: d.weight,
+    };
+  });
+
+  result?.sort(function (a: ChartData, b: ChartData) {
+    if (a.date === b.date) {
+      return 0;
+    } else if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+
+  return result;
+};
+
+// activeIndexからグラフに表示する日数を取得する
+export const getGraphPeriodFromActiveIndex = (activeIndex: number): GraphPeriodValue => {
+  switch (activeIndex) {
+    case 0:
+      return GRAPH_PERIOD['WEEK'];
+    case 1:
+      return GRAPH_PERIOD['MONTH'];
+    case 2:
+      return GRAPH_PERIOD['YEAR'];
+    case 3:
+      return GRAPH_PERIOD['ALL'];
+    default:
+      throw new Error('wrong activeIndex given');
+  }
+};
+
 const CONST = {
   ICON: {
     HEIGHT: '28px',
@@ -10,7 +90,7 @@ const CONST = {
   },
 };
 
-const getIcon = (title: PageTitle, isActive?: boolean): JSX.Element | undefined => {
+export const getIcon = (title: PageTitle, isActive?: boolean): JSX.Element | undefined => {
   const TITLE: string[] = Object.keys(PAGE_PATH);
 
   switch (title) {
@@ -27,7 +107,8 @@ const getIcon = (title: PageTitle, isActive?: boolean): JSX.Element | undefined 
   }
 };
 
-const getDateInfo = (date: Date) => {
+// Dateを項目毎にプロパティとして分けたオブジェクトに変換して返す
+export const getDateInfo = (date: Date) => {
   const weekdayIndex: number = date.getDay();
   const dateInfo = {
     weekday: WEEK_DAYS[weekdayIndex],
@@ -39,7 +120,8 @@ const getDateInfo = (date: Date) => {
   return dateInfo;
 };
 
-const getStringTypeDate = (date: Date, outputFormat?: 'YYYY-MM-DD' | 'normal'): string => {
+// Date型からString型に変換したものを返す
+export const getStringTypeDate = (date: Date, outputFormat?: 'YYYY-MM-DD' | 'normal'): string => {
   const year = date.getFullYear();
   if (outputFormat === 'YYYY-MM-DD') {
     const mm = ('00' + (date.getMonth() + 1)).slice(-2);
@@ -53,12 +135,10 @@ const getStringTypeDate = (date: Date, outputFormat?: 'YYYY-MM-DD' | 'normal'): 
 };
 
 // 初期値と最大値と差分からnumber型の配列を作成し返す
-const getNumArr = (init: number, max: number, diff: number): number[] => {
+export const getNumArr = (init: number, max: number, diff: number): number[] => {
   const numArr: number[] = [];
   for (let i = init; i <= max; i = i + diff) {
     numArr.push(i);
   }
   return numArr;
 };
-
-export { getIcon, getDateInfo, getStringTypeDate, getNumArr };
