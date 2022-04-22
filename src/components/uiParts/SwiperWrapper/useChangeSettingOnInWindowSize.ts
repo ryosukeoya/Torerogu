2cc8d81c284eeed50709fcdd64d897144956252a
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-04-22 21:01:14 +0900

    修正

--- src/components/uiParts/SwiperWrapper/useChangeSettingOnInWindowSize.ts
+++ src/components/uiParts/SwiperWrapper/useChangeSettingOnInWindowSize.ts
@@ -1,40 +1,54 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import Swiper, { EffectFade, SwiperOptions } from 'swiper';
-import { useSetRecoilState, useRecoilState } from 'recoil';
-import { mainTabIndexAtom, swiperAtom } from '~/store/atoms';
+import { useSetRecoilState, useRecoilState, useRecoilValue } from 'recoil';
+import { mainTabIndexAtom, pageIndexAtom, swiperAtom } from '~/store/atoms';
 import { useGetWindowSize } from '~/hooks';
 import { BREAKPOINT } from '~/styles/const';
 
 export const useChangeSettingOnInWindowSize = (): void => {
   const [swiper, setSwiper] = useRecoilState<Swiper | undefined>(swiperAtom);
   const setActiveIndex = useSetRecoilState<number>(mainTabIndexAtom);
+  const index = useRecoilValue<number>(pageIndexAtom);
   const windowSize = useGetWindowSize();
 
+  const mounted = useRef(false);
+
   useEffect(() => {
-    const options: SwiperOptions = {
-      simulateTouch: false,
-      fadeEffect: { crossFade: true },
-      nested: true,
-      on: {
-        realIndexChange: function (swiper) {
-          setActiveIndex(swiper.realIndex);
+    const set = (isMounted: boolean) => {
+      const options: SwiperOptions = {
+        simulateTouch: false,
+        fadeEffect: { crossFade: true },
+        nested: true,
+        on: {
+          realIndexChange: function (swiper) {
+            setActiveIndex(swiper.realIndex);
+          },
         },
-      },
+      };
+
+      const realIndex = swiper?.realIndex;
+
+      if (windowSize.width >= BREAKPOINT.MD) {
+        options.effect = 'fade';
+        options.speed = 230;
+        Swiper.use([EffectFade]);
+      } else {
+        options.effect = 'slide';
+        options.speed = 350;
+      }
+      options.initialSlide = realIndex;
+      if(!isMounted) options.initialSlide = index;
+      swiper?.destroy(false, true);
+      const newSwiper = new Swiper('.swiper-container', options);
+      setSwiper(newSwiper);
     };
 
-    const realIndex = swiper?.realIndex;
-    if (windowSize.width >= BREAKPOINT.MD) {
-      options.effect = 'fade';
-      options.speed = 230;
-      Swiper.use([EffectFade]);
+    if (mounted.current) {
+      set(mounted.current);
     } else {
-      options.effect = 'slide';
-      options.speed = 350;
+      set(mounted.current);
+      mounted.current = true;
     }
-    options.initialSlide = realIndex;
-    swiper?.destroy(false, true);
-    const newSwiper = new Swiper('.swiper-container', options);
-    setSwiper(newSwiper);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [windowSize]);
 };
