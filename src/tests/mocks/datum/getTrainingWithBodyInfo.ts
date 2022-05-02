b15801a193a5a3ebc36545385e0180a054b5fafe
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-05-03 01:43:52 +0900

    修正

--- src/tests/mocks/datum/getTrainingWithBodyInfo.ts
+++ src/tests/mocks/datum/getTrainingWithBodyInfo.ts
@@ -39,22 +39,5 @@ export const GetTrainingWithBodyMock = {
         },
       ],
     },
-    // trainings(order_by: {id: asc}) {
-    //     id
-    //     user_id
-    //     training_type_id
-    //     training_weight
-    //     training_count
-    //     training_set
-    //     is_finish
-    //     date
-    //   }
-    //  body_info_data_histories(order_by: {id: asc}) {
-    //     id
-    //     user_id
-    //     weight
-    //     date
-    //     is_record
-    //   }
   },
 };
