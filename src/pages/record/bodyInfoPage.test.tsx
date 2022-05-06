8e3bd4952b191ae1f83be02825c9b4852ab1cbc4
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-05-06 19:30:03 +0900

    bodyInfoPageのformのテスト追加

---
+++ src/pages/record/bodyInfoPage.test.tsx
@@ -0,0 +1,43 @@
+import BodyInfoPage from './BodyInfoPage';
+import { screen, act, fireEvent } from '@testing-library/react';
+import { testRendererUsingApolloClientMock } from '~/tests/mocks/renders/testRendererUsingApolloClientMock';
+import { createBodyInfoHistories } from '~/tests/mocks/datum/createBodyInfoHistories';
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      asPath: '/',
+    };
+  },
+}));
+jest.mock('~/components/PageSlider/useChangeSettingOnInWindowSize');
+
+describe('Integration Test', () => {
+  const renderPage = testRendererUsingApolloClientMock(<BodyInfoPage pageIndex={0} />, [createBodyInfoHistories]);
+  
+  it('必須項目が入力されている状態で送信ボタンをクリックした場合、Snackbarが表示される', async () => {
+    renderPage();
+    const submitButton = screen.getByTestId('submit');
+    const weightInput: HTMLInputElement = screen.getByTestId('weight');
+    fireEvent.input(weightInput, { target: { value: 55 } });
+    expect(weightInput.value).toBe('55');
+    fireEvent.submit(submitButton);
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 0));
+    });
+    expect(screen.getByText('記録しました！'));
+  });
+
+  it('必須項目が入力されていない状態で送信ボタンをクリックした場合、登録処理がが実行されず、Snackbarが表示されない', async () => {
+    renderPage();
+    const submitButton = screen.getByTestId('submit');
+    const bodyFatPercentageInput: HTMLInputElement = screen.getByTestId('bodyFatPercentage');
+    fireEvent.input(bodyFatPercentageInput, { target: { value: 20 } });
+    expect(bodyFatPercentageInput.value).toBe('20');
+    fireEvent.submit(submitButton);
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 0));
+    });
+    expect(() => screen.getByText('記録しました！')).toThrowError();
+  });
+});
