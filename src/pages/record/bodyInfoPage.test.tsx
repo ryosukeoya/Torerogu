265bc6b137ec9f88ee6bb8a00d694f63f12b6a31
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-05-06 21:27:34 +0900

    リファクタリング

--- src/pages/record/bodyInfoPage.test.tsx
+++ src/pages/record/bodyInfoPage.test.tsx
@@ -1,7 +1,7 @@
 import BodyInfoPage from './BodyInfoPage';
 import { screen, act, fireEvent } from '@testing-library/react';
 import { testRendererUsingApolloClientMock } from '~/tests/mocks/renders/testRendererUsingApolloClientMock';
-import { createBodyInfoHistories } from '~/tests/mocks/datum/createBodyInfoHistories';
+import { createBodyInfoHistories, recordPageVariables } from '~/tests/mocks/datum/createBodyInfoHistoriesMock';
 
 jest.mock('next/router', () => ({
   useRouter() {
@@ -13,9 +13,9 @@ jest.mock('next/router', () => ({
 jest.mock('~/components/PageSlider/useChangeSettingOnInWindowSize');
 
 describe('Integration Test', () => {
-  const renderPage = testRendererUsingApolloClientMock(<BodyInfoPage pageIndex={0} />, [createBodyInfoHistories]);
+  const renderPage = testRendererUsingApolloClientMock(<BodyInfoPage pageIndex={0} />, [createBodyInfoHistories(recordPageVariables)]);
   
-  it('必須項目が入力されている状態で送信ボタンをクリックした場合、Snackbarが表示される', async () => {
+  it('必須項目が入力されている状態で送信ボタンをクリックした場合、スナックバーが表示される', async () => {
     renderPage();
     const submitButton = screen.getByTestId('submit');
     const weightInput: HTMLInputElement = screen.getByTestId('weight');
@@ -28,7 +28,7 @@ describe('Integration Test', () => {
     expect(screen.getByText('記録しました！'));
   });
 
-  it('必須項目が入力されていない状態で送信ボタンをクリックした場合、登録処理がが実行されず、Snackbarが表示されない', async () => {
+  it('必須項目が入力されていない状態で送信ボタンをクリックした場合、登録処理がが実行されず、スナックバーが表示されない', async () => {
     renderPage();
     const submitButton = screen.getByTestId('submit');
     const bodyFatPercentageInput: HTMLInputElement = screen.getByTestId('bodyFatPercentage');
