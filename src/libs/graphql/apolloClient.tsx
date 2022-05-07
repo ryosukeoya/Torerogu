6e2c11aaf40d7475f09e8e5d5ca4199888f56bad
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-05-07 23:11:09 +0900

    ADMIN_SECRETの設定追加

--- src/libs/graphql/apolloClient.tsx
+++ src/libs/graphql/apolloClient.tsx
@@ -8,6 +8,9 @@ const createApolloClient = () => {
     ssrMode: typeof window === 'undefined',
     link: new HttpLink({
       uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+      headers: {
+        'x-hasura-admin-secret': process.env.NEXT_PUBLIC_HASURA_GRAPHQL_ADMIN_SECRET,
+      },
     }),
     cache: new InMemoryCache(),
   });
