b30032569e6a3ae0f9eb7d49a38a31defdf92232
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-05-14 03:32:45 +0900

    user_id削除

--- src/libs/graphql/mutations/index.ts
+++ src/libs/graphql/mutations/index.ts
@@ -1,16 +1,16 @@
 import { gql } from '@apollo/client';
 
 export const CREATE_BODY_INFO_HISTORIES = gql`
-  mutation CreateBodyInfoHistories($height: numeric, $weight: numeric!, $body_fat_percentage: Int, $date: timestamptz!, $user_id: Int!, $is_record: Boolean!) {
-    insert_body_info_data_histories_one(object: { height: $height, body_fat_percentage: $body_fat_percentage, date: $date, weight: $weight, user_id: $user_id, is_record: $is_record }) {
+  mutation CreateBodyInfoHistories($height: numeric, $weight: numeric!, $body_fat_percentage: Int, $date: timestamptz!, $is_record: Boolean!) {
+    insert_body_info_data_histories_one(object: { height: $height, body_fat_percentage: $body_fat_percentage, date: $date, weight: $weight, is_record: $is_record }) {
       id
     }
   }
 `;
 
 export const CREATE_TRAINING = gql`
-  mutation CreateTraining($user_id: Int!, $training_type_id: Int!, $training_weight: numeric!, $training_count: Int!, $training_set: Int!, $is_finish: Boolean!, $date: date!) {
-    insert_trainings_one(object: { user_id: $user_id, training_type_id: $training_type_id, training_weight: $training_weight, training_count: $training_count, training_set: $training_set, is_finish: $is_finish, date: $date }) {
+  mutation CreateTraining($training_type_id: Int!, $training_weight: numeric!, $training_count: Int!, $training_set: Int!, $is_finish: Boolean!, $date: date!) {
+    insert_trainings_one(object: { training_type_id: $training_type_id, training_weight: $training_weight, training_count: $training_count, training_set: $training_set, is_finish: $is_finish, date: $date }) {
       id
     }
   }
