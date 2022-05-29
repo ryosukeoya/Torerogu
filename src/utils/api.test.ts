f72a8427fd00b348df9c3cd18bb5d6c317309b97
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-05-30 02:07:44 +0900

    utils/apiののテスト修正

--- src/utils/api.test.ts
+++ src/utils/api.test.ts
@@ -1,4 +1,4 @@
-import { getDataSpecifiedDate, getDateChangedSpecifiedDaysPart, getStringTypeDate, getExtractedDataLaterThanTheSpecifiedDate, getDateBeforeOneDay } from '~/utils';
+import { getDataSpecifiedDate, getExtractedDataLaterThanTheSpecifiedDate } from '~/utils/api';
 
 describe('Unit Test', () => {
   test('指定した日付のデータが取得できる', () => {
@@ -9,21 +9,21 @@ describe('Unit Test', () => {
 
   test('終了フラグがfalseの場合は指定した日付以降のデータと終了フラグがtrueの場合は全てのデータの条件で抽出されたデータを取得できる', () => {
     const testData = [
-      { date: getStringTypeDate(getDateChangedSpecifiedDaysPart(new Date(), - 40), 'YYYY-MM-DD'), is_finish: false },
-      { date: getStringTypeDate(getDateChangedSpecifiedDaysPart(new Date(), - 40), 'YYYY-MM-DD'), is_finish: true },
-      { date: getStringTypeDate(getDateChangedSpecifiedDaysPart(new Date(), - 1), 'YYYY-MM-DD'), is_finish: false },
-      { date: getStringTypeDate(new Date(), 'YYYY-MM-DD'), is_finish: false },
-      { date: getStringTypeDate(getDateChangedSpecifiedDaysPart(new Date(), 1), 'YYYY-MM-DD'), is_finish: false },
-      { date: getStringTypeDate(getDateChangedSpecifiedDaysPart(new Date(), 40), 'YYYY-MM-DD'), is_finish: false },
-      { date: getStringTypeDate(getDateChangedSpecifiedDaysPart(new Date(), 40), 'YYYY-MM-DD'), is_finish: true },
+      { date: '2022-04-28 08:00:00', is_finish: false },
+      { date: '2022-04-28 08:00:00', is_finish: true },
+      { date: '2022-04-29 08:00:00', is_finish: false },
+      { date: '2022-05-30 08:00:00', is_finish: false },
+      { date: '2022-05-31 08:00:00', is_finish: false },
+      { date: '2022-06-28 08:00:00', is_finish: false },
+      { date: '2022-06-28 08:00:00', is_finish: true },
     ];
 
-    expect(getExtractedDataLaterThanTheSpecifiedDate(testData, getDateBeforeOneDay(new Date()))).toEqual([
-      testData[1],
-      testData[3],
-      testData[4],
-      testData[5],
-      testData[6],
+    expect(getExtractedDataLaterThanTheSpecifiedDate(testData, new Date('2022-05-30 08:00:00'))).toEqual([
+      { date: '2022-04-28 08:00:00', is_finish: true },
+      { date: '2022-05-30 08:00:00', is_finish: false },
+      { date: '2022-05-31 08:00:00', is_finish: false },
+      { date: '2022-06-28 08:00:00', is_finish: false },
+      { date: '2022-06-28 08:00:00', is_finish: true },
     ]);
   });
 });
